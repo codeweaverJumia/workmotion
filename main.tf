3cfd7062d59e0d9405c3a451adb7361900e9f27c
codeweaverJumia <94427177+codeweaverJumia@users.noreply.github.com>
2021-12-31 08:19:05 +0100

    added module

--- main.tf
+++ main.tf
@@ -3,19 +3,3 @@ provider "aws" {
 }
 data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
-
-
-module "lambda" {
-  source = "github.com/codeweaverJumia/workmotion.git"
-
-  name = var.name
-
-  ci_container_name = var.ci_container_name
-  repo_owner        = var.repo_owner
-  repo_name         = var.repo_name
-  branch            = var.branch
-  main_domain       = var.main_domain
-  zone_id           = var.zone_id
-  certificate_arn   = var.certificate_arn
-  env_vars          = var.env_vars
-}
---
+++ modules/lambda.tf
@@ -0,0 +1,14 @@
+module "lambda" {
+  source = "github.com/codeweaverJumia/workmotion.git"
+
+  name = var.name
+
+  ci_container_name = var.ci_container_name
+  repo_owner        = var.repo_owner
+  repo_name         = var.repo_name
+  branch            = var.branch
+  main_domain       = var.main_domain
+  zone_id           = var.zone_id
+  certificate_arn   = var.certificate_arn
+  env_vars          = var.env_vars
+}

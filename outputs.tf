8946fbfc12b715715d5261c06c792542ebd358b0
codeweaverJumia <94427177+codeweaverJumia@users.noreply.github.com>
2021-12-31 08:37:20 +0100

    summary

--- outputs.tf
+++ outputs.tf
@@ -5,7 +5,3 @@ output "function_arn" {
 output "function_name" {
   value = aws_lambda_function.service.function_name
 }
-
-output "base_url" {
-  value = aws_api_gateway_deployment.api.invoke_url
-}
